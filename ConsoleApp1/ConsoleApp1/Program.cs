cf665ce8ee494dad0afa2ede07df20bfe1a3977d
PlatonKlb <57142723+PlatonKlb@users.noreply.github.com>
2019-11-14 17:17:17 +0300

    Внесли правки

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -17,56 +17,76 @@ using System.Threading.Tasks;namespace ConsoleApp1
         {
             string input = "";
 
-            Console.WriteLine("\tВыберите функцию:" +
+            while(input != "stop")
+            {
+                Console.Clear();
+                Console.Write($"Студент: ID: {id} ФИО: {name} Дата рождения: {dateOfBirth.ToString("dd.MM.yyyy")} Институт: {instute} Курс: {course} Группа: {group} Средний балл: {averagePoints}\n\n");
+                Console.WriteLine("\tКакую информацию хотите изменить?\n" +
                 "\n 1. ФИО" +
                 "\n 2. Дата рождения" +
                 "\n 3. Институт" +
                 "\n 4. Курс" +
                 "\n 5. Группа" +
                 "\n 6. Средний балл" +
-                "\n 7. Введите stop для выхода из этого меню");
+                "\n 7. Выход из этого меню");
 
-            while(input != "stop")
-            {
+                Console.Write("\nВведите номер функции: ");
                 input = Console.ReadLine();
-                switch(input)
+                
+                switch (input)
                 {
                     case "1":
-                        Console.Write("Введите новые данные ");
+                        Console.Write("Введите новые данные: ");
                         name = Console.ReadLine();
                         Console.Write("Данные студента успешно обновлены\n");
                         break;
                     case "2":
-                        Console.Write("Введите новые данные ");
-                        dateOfBirth = DateTime.Parse(Console.ReadLine());
-                        Console.Write("Данные студента успешно обновлены\n");
+                        try
+                        {
+                            Console.Write("Введите новые данные: ");
+                            dateOfBirth = DateTime.Parse(Console.ReadLine());
+                            Console.Write("Данные студента успешно обновлены\n");
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Не коректный ввод данных!");
+                        }
                         break;
                     case "3":
-                        Console.Write("Введите новые данные ");
+                        Console.Write("Введите новые данные: ");
                         instute = Console.ReadLine();
                         Console.Write("Данные студента успешно обновлены\n");
                         break;
                     case "4":
-                        Console.Write("Введите новые данные ");
+                        Console.Write("Введите новые данные: ");
                         course = Console.ReadLine();
                         Console.Write("Данные студента успешно обновлены\n");
                         break;
                     case "5":
-                        Console.Write("Введите новые данные ");
+                        Console.Write("Введите новые данные: ");
                         group = Console.ReadLine();
                         Console.Write("Данные студента успешно обновлены\n");
                         break;
                     case "6":
-                        Console.Write("Введите новые данные ");
-                        averagePoints = float.Parse(Console.ReadLine());
-                        Console.Write("Данные студента успешно обновлены\n");
+                        try
+                        {
+
+                            averagePoints = float.Parse(Console.ReadLine());
+                            Console.Write("Данные студента успешно обновлены\n");
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Не коректный ввод данных!");                            
+                        }
                         break;
-                    case "stop":
-                        break;
+                    case "7":
+                        return;
                     default:            //Защита от дурака
-                        Console.WriteLine("Не коректный ввод данных, введите число!");
+                        Console.WriteLine("Не коректный ввод данных!");
                         break;
                 }
+                Console.WriteLine("Нажмите любую клавишу для продолжения");
+                Console.ReadKey();
             }
         }        public string GetStudentInfo()          //Функция отвечает за формат данных о студенте
         {
@@ -81,7 +101,10 @@ using System.Threading.Tasks;namespace ConsoleApp1
 
         public void Menu()          //Основное меню
         {
-            string input = "";            Console.WriteLine("\tЗдравствуйте! Введите номер функции или \"stop\" для выхода из программы\n" +
+            string input = "";            while (input != "stop")
+            {
+                Console.Clear();
+                Console.WriteLine("\tЗдравствуйте! Введите номер функции\n" +
                 "\n1. Вывести список студентов" +
                 "\n2. Добавление студентов" +
                 "\n3. Сортировка по ФИО" +
@@ -94,13 +117,15 @@ using System.Threading.Tasks;namespace ConsoleApp1
                 "\n10. Нахождение минимального среднего балла" +
                 "\n11. Нахождение среднего балла" +
                 "\n12. Нахождение суммы средних баллов" +
-                "\n13. Сохранить данные в файл");            while (input != "stop")
-            {
+                "\n13. Сохранить данные в файл" +
+                "\n14. Редактирование студентов" +
+                "\n15. Выход из программы");
+
                 Console.Write("\nВведите номер функции: ");
                 input = Console.ReadLine();                switch (input)
                 {
                     case "1":
-                        StudentsMenu(students);
+                        Print(students);
                         break;
                     case "2":
                         Add(StudentsParseService.GetStudents());
@@ -128,10 +153,12 @@ using System.Threading.Tasks;namespace ConsoleApp1
                         StudentsMenu(Search(x => x.dateOfBirth == DateTime.Parse(input)));
                         break;
                     case "9":
-                        Max();
+                        float max = students.Max(y => y.averagePoints);
+                        Print(Search(x => x.averagePoints == max));
                         break;
                     case "10":
-                        Min();
+                        float min = students.Min(y => y.averagePoints);
+                        Print(Search(x => x.averagePoints == min));
                         break;
                     case "11":
                         Average();
@@ -142,14 +169,22 @@ using System.Threading.Tasks;namespace ConsoleApp1
                     case "13":
                         SaveToFile();
                         break;
+                    case "14":
+                        StudentsMenu(students);
+                        break;
+                    case "15":
+                        return;
                     default:            //Защита от дурака
                         Console.WriteLine($"Упс! Функция \"{input}\" не найдена");
                         break;
                 }
+                Console.WriteLine("Нажмите любую клавишу для продолжения");
+                Console.ReadKey();
             }
         }
         public void StudentsMenu(List<Student> students)            //Функция вызывающая личное меню студента
         {
+            Console.Clear();
             Print(students);
 
             if (students.Count == 0)
@@ -157,45 +192,43 @@ using System.Threading.Tasks;namespace ConsoleApp1
 
             string input = "";
 
-            while(input != "stop")
-            {
-                Console.Write("Выберите студента: ");
-                input = Console.ReadLine();
+            Console.Write("Введите ID студента: ");
+            input = Console.ReadLine();
 
-                try
-                {
-                    int index = int.Parse(input);
-                    PodMenu(students[index]);
-                    break;
-                }
-                catch
-                {
-                    Console.WriteLine("Не коректный ввод данных, введите число!");
-                    break;
-                }
+            try
+            {
+                PodMenu(students.FirstOrDefault(x => x.id == input));
+            }
+            catch
+            {
+                Console.WriteLine("Не коректный ввод данных!");
             }
         }
         public void PodMenu(Student student)            //Личное меню студента
         {
+            Console.Clear();
             string input = "";
             Int32 n;
             Console.WriteLine($"Что вы хотите сделать с {student.name}?" +
                 "\n1. Изменить" +
-                "\n2. Удалить" +
-                "\n3. Напишите stop для выхода в основное меню");
+                "\n2. Удалить");
 
-            while (input != "stop")
+            Console.Write("\nВведите номер функции: ");
+            input = Console.ReadLine();
+
+            switch (input)
             {
-                Console.Write("\nВведите номер функции: ");
-                input = Console.ReadLine();
-
-                switch (input)
-                {
-                    case "1":                        student.Modify();                        return;
-                    case "2":                        Delete(student);                        return;
-                    case "stop":
-                        break;
-                    default:                        Console.WriteLine("Не коректный ввод данных, введите число!");                        break;                }
+                case "1":
+                    student.Modify();
+                    return;
+                case "2":
+                    Delete(student);
+                    return;
+                case "stop":
+                    break;
+                default:
+                    Console.WriteLine("Не коректный ввод данных!");
+                    break;
             }
         }
         void Add(List<Student> students)            //Добавление студента(ов)
@@ -231,43 +264,12 @@ using System.Threading.Tasks;namespace ConsoleApp1
             float average = 0;            foreach (Student s in students)
                 average += s.averagePoints;            average = average / students.Count;
             Console.WriteLine($"Средний балл: {average}");
-        }        void Min()          //Поиск минимального балла
-        {
-            float min = 0;
-            try
-            {
-                min = students.Min(x => x.averagePoints);
-            }
-            catch
-            {
-                Console.WriteLine("Невозможно выполнить функцию. Список пуст");
-            }
-            finally
-            {
-                Console.WriteLine($"Минимальный балл: {min}");
-            }
-        }        void Max()          //Поиск максимального балла
-        {
-            float max = 0;
-            try
-            {
-                max = students.Max(x => x.averagePoints);
-            }
-            catch
-            {
-                Console.WriteLine("Невозможно выполнить функцию. Список пуст");
-            }
-            finally
-            {
-                Console.WriteLine($"Максимальный балл: {max}");
-            }
         }        void Sum()          //Сумма средних баллов
         {
             float sum = 0;            foreach (Student s in students)
                 sum += s.averagePoints;            Console.WriteLine($"Сумма средних баллов: {sum}");
         }        void SaveToFile()           //Сохранение в файл
-        {
-            string input;            Console.Write("Введите путь к файлу: ");            input = Console.ReadLine();            File.WriteAllText(input, GetAllStudentsInfo());
+        {            File.WriteAllText("C:\\Users\\pavel\\Desktop\\test.txt", GetAllStudentsInfo());
             Console.WriteLine("Информация о студентах успешно сохранена");
         }        string GetAllStudentsInfo()         //Переводим в строки
         {
@@ -284,17 +286,14 @@ using System.Threading.Tasks;namespace ConsoleApp1
             return allInfo;
         }        void Print(List<Student> students)          //Вывод
         {
-            for (int i = 0; i < students.Count; i++)
-            {
-                Console.Write($"{i}.\t");
-                students[i].Print();
-            }            if (students.Count == 0)
+            foreach (Student s in students)                s.Print();            if (students.Count == 0)
                 Console.WriteLine("Список пуст!");
         }
     }    public static class StudentsParseService            //Сервис ввода студентов
     {
         public static List<Student> GetStudents()
         {
+            Console.Clear();
             Console.WriteLine("\tКак вы хотите ввести данные?" +
                 "\n1. С клавиатуры." +
                 "\n2. Из файла");            List<Student> students = new List<Student>();            string input = Console.ReadLine();            switch (input)
@@ -319,10 +318,9 @@ using System.Threading.Tasks;namespace ConsoleApp1
                     array += "\r\n";
             }            return array;
         }        static string FileInput()           //Ввод с файла
-        {
-            string input;            Console.Write("Введите путь к файлу: ");            input = Console.ReadLine();            string text = "";            try
+        {            string text = "";            try
             {
-                text = File.ReadAllText(input);
+                text = File.ReadAllText("C:\\Users\\pavel\\Desktop\\test.txt");
             }
             catch
             {
@@ -368,5 +366,4 @@ using System.Threading.Tasks;namespace ConsoleApp1
         }
     }
 }
-//Один файл
-//Оптимизировать под пользователя
+// 14. пункт передвинуть в начало (2)
